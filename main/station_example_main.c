502aba5b0832de4d7e4e59cd24bf58a6beffe09e
romero-jose <jose.romero@ing.uchile.cl>
2021-11-22 00:29:47 -0300

    Make http request

--- main/station_example_main.c
+++ main/station_example_main.c
@@ -7,6 +7,7 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include "esp_event.h"
+#include "esp_http_client.h"
 #include "esp_log.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
@@ -43,7 +44,8 @@ static const char *TAG = "wifi station";
 
 static int s_retry_num = 0;
 
-static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data) {
   if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
     esp_wifi_connect();
   } else if (event_base == WIFI_EVENT &&
@@ -130,6 +132,55 @@ void wifi_init_sta(void) {
   vEventGroupDelete(s_wifi_event_group);
 }
 
+esp_err_t _http_event_handle(esp_http_client_event_t *evt) {
+  switch (evt->event_id) {
+  case HTTP_EVENT_ERROR:
+    ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
+    break;
+  case HTTP_EVENT_ON_CONNECTED:
+    ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
+    break;
+  case HTTP_EVENT_HEADER_SENT:
+    ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
+    break;
+  case HTTP_EVENT_ON_HEADER:
+    ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER");
+    printf("%.*s", evt->data_len, (char *)evt->data);
+    break;
+  case HTTP_EVENT_ON_DATA:
+    ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+    if (!esp_http_client_is_chunked_response(evt->client)) {
+      printf("%.*s", evt->data_len, (char *)evt->data);
+    }
+
+    break;
+  case HTTP_EVENT_ON_FINISH:
+    ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
+    break;
+  case HTTP_EVENT_DISCONNECTED:
+    ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+    break;
+  }
+  return ESP_OK;
+}
+
+void send_data() {
+  esp_http_client_config_t config = {
+      .url = "http://httpbin.org/redirect/2",
+      .event_handler = _http_event_handle,
+  };
+  esp_http_client_handle_t client = esp_http_client_init(&config);
+  esp_err_t err = esp_http_client_perform(client);
+
+  if (err == ESP_OK) {
+    ESP_LOGI(TAG, "Status = %d, content_length = %d",
+             esp_http_client_get_status_code(client),
+             esp_http_client_get_content_length(client));
+  }
+  esp_http_client_cleanup(client);
+  ESP_LOGI(TAG, "HTTP request sent\n");
+}
+
 void app_main(void) {
   // Initialize NVS
   esp_err_t ret = nvs_flash_init();
@@ -144,4 +195,6 @@ void app_main(void) {
   wifi_init_sta();
 
   ESP_LOGI(TAG, "Successfuly connected to WiFi!\n");
+
+  send_data();
 }
